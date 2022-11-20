8406878463e93c0301568b336834a4a1a89fc9d6
bipulcn <bipulcn@gmail.com>
2022-11-20 19:10:42 +0600

    fist initiated

---
+++ src/App.svelte
@@ -0,0 +1,8 @@
+<script>
+  import { Router, Route, Link } from "svelte-routing";
+  import Navbar from "./route/layout/navbar.svelte";
+  import Page1 from "./route/taliwind.svelte"
+</script>
+
+<Navbar />
+<Page1 />
---
+++ src/route/taliwind.svelte
@@ -0,0 +1,26 @@
+
+<div class="text-5xl font-extrabold text-center py-3">
+    <span class="bg-clip-text text-transparent bg-gradient-to-r from-red-500 to-purple-500">
+      Hello world
+    </span>
+  </div>
+  
+  <div class="flex">
+    <div class="flex-none w-16 h-12 bg-sky-500 text-center py-3">01</div>
+    <div class="flex-auto bg-orange-400 text-center py-3">02</div>
+    <div class="flex-initial w-32 bg-purple-400 text-center py-3">03</div>
+  </div>
+  
+  <button class="bg-emerald-600 px-4 py-2 text-white rounded-md shadow-sm shadow-emerald-700 hover:bg-sky-800 sm:px-8 sm:py-3 mx-3 my-3">tailwind</button>
+  
+  <div class="bg-sky-100 mix-blend-multiply text-black p-5 bg-blend-lighten hover:bg-blend-darken scale-90 hover:scale-100 transition-all" > test</div>
+  
+  <div class="columns-2 bg-rose-700 p-3 my-3">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe vitae adipisci debitis eius mollitia. Aperiam iure obcaecati cum ab. Voluptate quo nesciunt enim ipsam perferendis at aperiam! Fugiat, maiores repudiandae. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe vitae adipisci debitis eius mollitia. Aperiam iure obcaecati cum ab. Voluptate quo nesciunt enim ipsam perferendis at aperiam! Fugiat, maiores repudiandae.</div>
+  
+  
+  <div class="grid grid-cols-2 gap-4 w-1/2 m-auto place-content-center h-48">
+    <div class="bg-purple-400 mix-blend-multiply text-center p-3 rounded-md shadow-lg">01</div>
+    <div class="bg-purple-400 mix-blend-multiply text-center p-3 rounded-md shadow-lg">02</div>
+    <div class="bg-purple-400 mix-blend-multiply text-center p-3 rounded-md shadow-lg">03</div>
+    <div class="bg-purple-400 mix-blend-multiply text-center p-3 rounded-md shadow-lg">04</div>
+  </div>
